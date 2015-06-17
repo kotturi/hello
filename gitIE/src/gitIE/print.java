7d033a4cab76a17e0b2624b7aa728c8c3afaea69
kotturi <jo.kothuri@gmail.com>
2015-06-17 15:53:31 +0530

    first commit

---
+++ gitIE/src/gitIE/print.java
@@ -0,0 +1,12 @@
+package gitIE;
+
+public class print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello weclome to git import &export");
+
+	}
+
+}
